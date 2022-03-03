c66e08bea21d0d2a8df1342d60322b842a9317cc
vicceledon <vicceledon@gmail.com>
2022-03-03 20:35:40 -0300

    definicion de ruta estatica

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require("path");
 const ruta = path.resolve(__dirname);
 
+app.use(express.static(path.join(ruta, "/public")));
+
 app.listen(3000, () => {
   console.log("Servidor corriendo en el puerto 3000");
 });
