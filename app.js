390f8e5330b7755d84508eaffd700af4425c104c
vicceledon <vicceledon@gmail.com>
2022-03-24 20:15:24 -0300

    cambio nodemon por node para hacer deply

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const ruta = path.resolve(__dirname);
+const PORT = process.env.PORT || 4001;
 
 app.use(express.static(path.join(ruta, "/public")));
 
-app.listen(3000, () => {
+app.listen(4000, () => {
   console.log("Servidor corriendo en el puerto 3000");
 });
 
