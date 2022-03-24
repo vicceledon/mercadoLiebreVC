dd81b0340559b259967ba591672338489d6a5675
vicceledon <vicceledon@gmail.com>
2022-03-24 20:20:56 -0300

    cambio de variable 4000 a PORT

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static(path.join(ruta, "/public")));
 
-app.listen(4000, () => {
+app.listen(PORT, () => {
   console.log("Servidor corriendo en el puerto 3000");
 });
 
